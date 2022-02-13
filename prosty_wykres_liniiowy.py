112614189909e2c2a13e7263039d902815ab9e48
=R= <46672413+RadoslawP@users.noreply.github.com>
2022-02-13 13:36:51 +0100

    Add files via upload

--- prosty_wykres_liniiowy.py
+++ prosty_wykres_liniiowy.py
@@ -1,11 +1,16 @@
 from matplotlib import pyplot as plt
 
+
 years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
 
+
+# Tworzenie wykresu liniowego.
 plt.plot(years, gdp, color = 'green', marker = 'o', linestyle = 'solid')
 
+# Dodanie tytułu.
 plt.title("Nominalny PKB")
 
+# Dodanie tytułu osi y.
 plt.ylabel("Mld dol.")
 plt.show()
